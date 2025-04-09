6cc8ad4dbe6bb3fb81ca368417db57f96326b916
aalfath7 <abdielhafizhy@gmail.com>
2025-04-09 07:24:47 +0700

    edit utils server

--- server/utils/mongoose.ts
+++ server/utils/mongoose.ts
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 export const connectToDB = async () => {
   if (mongoose.connection.readyState === 1) return;
 
+  const config = useRuntimeConfig();
+  const uri: any = config.MONGODB_URI;
+
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/quran-tracking"
-    );
+    await mongoose.connect(uri);
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
